c371910e6554a93409d636e23fae7e95a3607fd6
Nandospto <nandospto@gmail.com>
2026-06-19 16:01:08 -0300

    fix: arrumado as variáveis do contato.js

--- js/contato.js
+++ js/contato.js
@@ -1,3 +1,6 @@
+const sugestionbox = document.getElementById('SugestionBox');
+const sugestionsuccess = document.getElementById('SuccessBox');
+
 function EnviarSugestao() {
     sugestionbox.style.display = "none";
     sugestionsuccess.classList.remove('success-box-hidden');
