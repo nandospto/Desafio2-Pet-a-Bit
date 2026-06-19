d28c19e24f5187511087ea4f59eeac111c2416e2
Gabryel Gomes Silva Souza <gabryelgomes3030@gmail.com>
2026-06-18 21:23:41 -0300

    feat: integra blog com a API para listar, criar e deletar posts

--- menu.js
+++ menu.js
@@ -2,8 +2,6 @@
 const btnMenu = document.getElementById('btn-menu');
 const navMenu = document.querySelector('#nav-menu ul');
 
-// Ouve o clique no botão
 btnMenu.addEventListener('click', () => {
-    // A função toggle liga e desliga a classe 'ativo' no menu
     navMenu.classList.toggle('ativo');
 });
